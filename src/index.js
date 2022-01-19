c9fa68f87720c5576f4f3fa90a59debb9a1a7125
Diego Perozo <diegodapt18@gmail.com>
2022-01-19 17:27:35 -0400

    changing the BrowserRouter for the HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { /*BrowserRouter,*/ HashRouter,  Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Skills from "./Skills";
 import Layout from "./components/Layout";
@@ -8,16 +8,15 @@ import Jobs from './Jobs';
 import Contact from './Contact';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <Layout>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="/react-responsive-portfolio/" element={<Home />} />
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/jobs" element={<Jobs />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="skills" element={<Skills />} />
+        <Route path="jobs" element={<Jobs />} />
+        <Route path="contact" element={<Contact />} />
       </Routes>
     </Layout>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
