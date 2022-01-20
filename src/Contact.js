73d2158fab9633a2ef178d54c69e50fa690baa8c
Diego Perozo <diegodapt18@gmail.com>
2022-01-20 18:20:54 -0400

    changing the CV for a designed pdf

--- src/Contact.js
+++ src/Contact.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import './styles/Contact.scss'
-import img from './assets/terminal.png'
+import React from "react";
+import "./styles/Contact.scss";
+import img from "./assets/terminal.png";
 
-export default function Contact(){
-  return(
+export default function Contact() {
+  return (
     <section class="contact">
       <h1 className="display-4 mb-3">
         <span> Contact</span>
@@ -18,13 +18,21 @@ export default function Contact(){
       </div>
       <div>
         <i class="fab fa-linkedin"></i>
-        <span> https://www.linkedin.com/in/diego-antonio-perozo-troncone-563893b9/</span>
+        <span>
+          {" "}
+          https://www.linkedin.com/in/diego-antonio-perozo-troncone-563893b9/
+        </span>
       </div>
       <div>
-        <a className="btn btn-primary mt-5" href="https://docs.google.com/document/d/1R-nprOIt_fgjLMcpDocfyFVeeRw33qFewRqdsFQrli4/edit?usp=sharing">Download CV</a>
+        <a
+          className="btn btn-primary mt-5"
+          href="https://drive.google.com/file/d/10JY2ObMEkO1asZ2FwFu7jDm4nMCH09Ly/view?usp=sharing"
+        >
+          Download CV
+        </a>
       </div>
 
-      <img className="img-fluid" src={img} alt="img"/>
+      <img className="img-fluid" src={img} alt="img" />
     </section>
-  )
+  );
 }
--- src/Home.js
+++ src/Home.js
@@ -8,7 +8,7 @@ export default function Home(){
       <h1 className="display-1 text-light">Fullstack Developer</h1>
       <h2 className="col-md-6 text-light opacity-50">
         I make user interfaces and application logic with javascript and react, as well as a lot of creativity.</h2>
-      <a className="btn btn-primary" href="https://docs.google.com/document/d/1R-nprOIt_fgjLMcpDocfyFVeeRw33qFewRqdsFQrli4/edit?usp=sharing">Download CV</a>
+      <a className="btn btn-primary" href="https://drive.google.com/file/d/10JY2ObMEkO1asZ2FwFu7jDm4nMCH09Ly/view?usp=sharing">Download CV</a>
     <div>
       <img className="img-fluid" src={img} alt="img"/>
     </div>
